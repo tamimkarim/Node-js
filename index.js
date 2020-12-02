5d9ad773d981132e1b178f7a059e8f2597ee18a8
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-12-03 01:37:21 +0600

    node final commit

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express');
-
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
-// const rootCall = (req, res) =>res.send('Thank you very much');
+app.use(cors());
+app.use(bodyParser.json());
+
+
+ const users = ["Asad", "Moin", "Tamim", "Karim", "Shoeb", "Anushka"];
 
 
 app.get('/', (req, res) =>{
-    res.send("I know how to open node")
-})
+    const fruit = {
+        product: 'Apple',
+        price: 220
+    }
+    
+    res.send(fruit);
+});
+app.get('/fruits/banana', (req, res) => {
 
-app.listen(3200, () => console.log('Listening to port 3000'));
+    res.send({fruit: 'banana' , quantity: 1000, price:1000} )
+ })
+
+ app.get('/users/:id', (req, res)=>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+
+ })
+
+ // post 
+ app.post('/addUser', (req, res) => {
+     console.log(req.body);
+
+
+ })
+
+
+app.listen(3000, () => console.log('Listening to port 3000'));
